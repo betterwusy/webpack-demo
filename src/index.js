ef469d9bceec45912c799add14018246b88a421d
吴胜业 <betterwusy@163.com>
2022-06-18 15:46:48 +0800

    config:添加webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import bar from './bar'
-bar();
+import _ from 'lodash'
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  return element;
+}
+
+document.body.appendChild(component());
